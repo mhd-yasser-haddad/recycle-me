a25195648c443a5784164984c202483b6c740e1a
MHD Yasser Haddad <yasserhaddad99@gmail.com>
2024-05-30 00:37:57 +0200

    Linking with the read barcode from image API

--- app/Http/Controllers/API/ProductController.php
+++ app/Http/Controllers/API/ProductController.php
@@ -2,36 +2,83 @@
 
 namespace App\Http\Controllers\API;
 
-use App\Http\Controllers\Controller;
-use App\Models\Material;
 use App\Models\Product;
+use App\Models\Material;
+
+use GuzzleHttp\Client;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class ProductController extends Controller
 {
-    public function getInstructions(Request $request){
+    public function getInstructions(Request $request)
+    {
         $data = $request->all();
+        $barcode = false;
 
-        // In case of an Image
+        // Get barcode from image or string
+        $image = $request->file('image');
+        if ($image) {
+            $imageBarcode = $this->getBarcode($image);
+            $imageBarcode = $imageBarcode['response']->barcodes;
+            if(is_string($imageBarcode) && $imageBarcode != ''){
+                $barcode = $imageBarcode;
+            } else {
+                return response()->json(['data' => [], 'message' => 'Unable to capture barcode from image, please try again'], 404);
+            }
+        } else if (isset($data['barcode']) && $data['barcode'] !== ''){
+            $barcode = $data['barcode'];
+        }
 
-        // In case of a code
-        $barcode = $data['barcode'];
+        // Check for a valid Barcode
+        if (!$barcode) {
+            return response()->json(['data' => [], 'message' => 'No image or barcode was provided'], 404);
+        }
 
+        // Search for its instructions in the database
         $product = Product::with('materials')->where('barcode', $barcode)->first();
-        if($product){
+        if ($product) {
             return response()->json(['data' => ['product' => $product], 'message' => 'Product retrieved successfully'], 200);
         } else {
             return response()->json(['data' => [], 'message' => 'Product is not in our system'], 404);
         }
-
     }
-    public function import(Request $request){
+
+    private function getBarcode($image)
+    {
+        // Create a Guzzle client
+        $client = new Client();
+
+        try {
+            // Send a POST request to the external API
+            $response = $client->post('https://recycleme-haris-ali-ae821b2b.koyeb.app/api/test', [
+                'multipart' => [
+                    [
+                        'name'     => 'image',
+                        'contents' => fopen($image->getPathname(), 'r'),
+                        'filename' => $image->getClientOriginalName(),
+                    ],
+                ],
+            ]);
+
+            // Get the response from the API
+            $responseBody = $response->getBody()->getContents();
+
+            // Return the response from the API
+            return ['response' => json_decode($responseBody)];
+        } catch (\Exception $e) {
+            // Handle errors
+            return ['error' => $e->getMessage()];
+        }
+    }
+    public function import(Request $request)
+    {
         $products = $request->all()['products'];
-        foreach($products as $product){
+        foreach ($products as $product) {
             $materialIDs = [];
-            foreach($product['material'] as $material){
+            foreach ($product['material'] as $material) {
                 $materialM = Material::where('name', $material)->first();
-                if($materialM){
+                if ($materialM) {
                     $materialIDs[] = $materialM->id;
                 } else {
                     $materialM = new Material();
@@ -40,7 +87,6 @@ class ProductController extends Controller
                     $materialM->refresh();
                     $materialIDs[] = $materialM->id;
                 }
-
             }
 
             $productM = new Product();
